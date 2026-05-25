0885244d0eccfbc7d0d645d84a48efa789d56c49
Nagendra <nagendra.burusu@thoughtclan.com>
2026-05-26 01:45:39 +0530

    If-else

---
+++ 02-constructs/01-if-else.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/*
+		no := 22
+
+		if no%2 == 0 {
+			fmt.Printf("%d is a even number\n", no)
+		} else {
+			fmt.Printf("%d is a Odd number\n", no)
+		}*/
+
+	if no := 22; no%2 == 0 {
+		fmt.Printf("%d is a even number\n", no)
+	} else {
+		fmt.Printf("%d is a Odd number\n", no)
+	}
+
+}
